88793407bccd7514d391280599089f99eebccd6e
Haddouche Maamar <frocodeof@gmail.com>
2024-05-04 00:09:34 +0200

    Trans unicorn

---
+++ Data Transformations/unicorn/stage1_clean_unicorn_data.sql
@@ -0,0 +1,16 @@
+SELECT * FROM dev.public.unicorn;
+ALTER TABLE dev.public.unicorn
+-----------------------------------------------------------------------------
+DROP COLUMN "company.1";
+
+------------------------------------------------------------------------------
+ALTER TABLE dev.public.unicorn RENAME COLUMN "valuation ($b)" TO valuation_inB;
+ALTER TABLE dev.public.unicorn RENAME COLUMN "date joined" TO date_joined;
+-------------------------------------------------------------------------------
+-- Update DT Year
+ALTER TABLE dev.public.unicorn ADD COLUMN year INTEGER;
+UPDATE dev.public.unicorn
+SET year = EXTRACT(YEAR FROM TO_DATE(date_joined, 'YYYY-MM-DD'));
+ALTER TABLE dev.public.unicorn DROP COLUMN new_year;
+ALTER TABLE dev.public.unicorn RENAME COLUMN new_year TO year;
+-----------------------------------------------------------------------------------
